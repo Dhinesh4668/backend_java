275543b1cff2ce70baa6347afe1508f506f5fa61
Dhinesh4668 <dhinesh4668@gmail.com>
2023-10-29 18:14:20 +0530

    basic routing

--- backend/src/main/java/com/example/backend/Test.java
+++ backend/src/main/java/com/example/backend/Test.java
@@ -5,9 +5,26 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Test {
-
+//home directry
     @GetMapping("/")
     public  String initial(){
         return "Api is working";
+    }
+//logn users page
+    @GetMapping("/login")
+    public String login(){
+        return  "this is the login page";
+    }
+
+//    product page
+    @GetMapping("/products")
+    public String product(){
+        return "this the product page";
+    }
+
+//    cart page
+    @GetMapping("/cart")
+    public  String cart(){
+        return "cart page";
     }
 }
